bc22666ec95d6fe7df3dabc1b23b51aeee55d7f8
Enrique Marín <enriquemarin_sierra@hotmail.com>
2025-03-22 16:00:45 +0100

    add tests for Ecto Changeset in the  schema

--- test/real_deal_api/schema/user_test.exs
+++ test/real_deal_api/schema/user_test.exs
@@ -1,4 +1,5 @@
 defmodule RealDealApi.Schema.UserTest do
+  alias Ecto.Changeset
   use RealDealApi.DataCase
 
   alias RealDealApi.Users.User
@@ -13,6 +14,8 @@ defmodule RealDealApi.Schema.UserTest do
     {:updated_at, :utc_datetime}
   ]
 
+  @optional [:id, :full_name, :gender, :biography, :inserted_at, :updated_at]
+
   describe "fields and types for`User` schema" do
     test "`User` schema has the correct fields and types" do
       actual_fields_with_types =
@@ -22,6 +25,82 @@ defmodule RealDealApi.Schema.UserTest do
         end
 
       assert MapSet.new(actual_fields_with_types) == MapSet.new(@expected_fields_with_types)
+    end
+  end
+
+  describe "changeset/2" do
+    test "success: returns a valid changeset when given valid arguments" do
+      valid_params = %{
+        "id" => Ecto.UUID.generate(),
+        "full_name" => "test name",
+        "gender" => "programmer",
+        "biography" => "a lifetime of coding",
+        "account_id" => Ecto.UUID.generate(),
+        "inserted_at" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "updated_at" => DateTime.utc_now() |> DateTime.truncate(:second)
+      }
+
+      changeset = User.changeset(%User{}, valid_params)
+
+      assert %Changeset{valid?: true, changes: changes} = changeset
+
+      for {field, _} <- @expected_fields_with_types do
+        actual = Map.get(changes, field)
+        expected = valid_params[Atom.to_string(field)]
+
+        assert actual == expected,
+               "Values did not match for field: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+      end
+    end
+
+    test "error: returns an error changeset when given un-castable values" do
+      invalid_params = %{
+        "id" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "full_name" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "gender" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "biography" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "account_id" => DateTime.utc_now() |> DateTime.truncate(:second),
+        "inserted_at" => "lets put a string here",
+        "updated_at" => "updated to a string"
+      }
+
+      assert %Changeset{valid?: false, errors: errors} =
+               User.changeset(%User{}, invalid_params)
+
+      for {field, _} <- @expected_fields_with_types do
+        assert errors[field], "The field: #{field} is misssing from errors."
+
+        {_, meta} = errors[field]
+
+        # We claim it is a casting error
+        assert meta[:validation] == :cast,
+               "The validation type, #{meta[:validation]}, is incorrect."
+      end
+    end
+
+    test "error: returns an error changeset when required fields are missing" do
+      params = %{}
+
+      assert %Changeset{valid?: false, errors: errors} =
+               User.changeset(%User{}, params)
+
+      #  We perform validation for non-optional (i.e. required) fields. ↓↓↓
+      for {field, _} <- @expected_fields_with_types, field not in @optional do
+        assert errors[field], "The field: #{field} is misssing from errors."
+
+        {_, meta} = errors[field]
+
+        # In this case, the error is not in the `casting`
+        # but rather that it is a required field.
+        assert meta[:validation] == :required,
+               "The validation type, #{meta[:validation]}, is incorrect."
+      end
+
+      for field <- @optional do
+        # If any optional field is present in the error list (not `nil`, i.e.
+        # not truthy) we throw an error (`refute` is the inverse of `assert`)
+        refute errors[field], "The optional field #{field} is required when it shouldn't be."
+      end
     end
   end
 end
